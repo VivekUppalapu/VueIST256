a73c03ad5c5ad160b1e07ff1466ab58508025546
VivekUppalapu <rrvivek16@gmail.com>
2023-09-17 20:15:52 -0400

    ...

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -31,38 +31,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.wrap{
-    width: 400px;
-    border: 2.5px solid black;
-    background-color: red;
-  }
-  .image{
-    width: 400px;
-    
-  }
-  .header{
-     text-align: center;
-  }
-  .header h3{
-    font-size: 40px;
-    color: blue;
-  }
-  .descrip
-  {
-    font-size: 16px;
-    font-weight: bold;
-    color: white;
-  }
-  .allbtns button
-  {
-    font-size: 15px;
-    background-color: white;
-    color: teal;
-  }
-  .Detail
-  {
-    font-size: 17px;
-    color: purple;
-    background-color: white;
-  }
 </style>
