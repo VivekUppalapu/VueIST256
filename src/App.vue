642fa6fc251ab28936fbac08b660cacc2e8298dd
VivekUppalapu <rrvivek16@gmail.com>
2023-09-17 21:33:50 -0400

    ..

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,3 @@
-<template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
@@ -13,13 +10,4 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
